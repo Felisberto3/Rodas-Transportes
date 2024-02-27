63d164312611bfec575b3c2761cb3ccd7ccf9878
Felisberto Alberto <felisbertoalberto183@gmail.com>
2024-02-28 00:31:31 +0100

    'update'

--- src/config/payLoad.ts
+++ src/config/payLoad.ts
@@ -1,11 +1,11 @@
 import JWT from "jsonwebtoken";
 
 interface payLoadDto {
-    userId: number,
+    id: number,
     email: string,
     mainAdmin: boolean
 }
-function payLoadGenerator({ email, mainAdmin, userId }: payLoadDto) {
+function payLoadGenerator({ email, mainAdmin, id:userId }: payLoadDto) {
     return JWT.sign(
         { email, mainAdmin, userId },
         process.env.KEY!,
--- src/module/secretario/useCases/create/createSecretarioController.ts
+++ src/module/secretario/useCases/create/createSecretarioController.ts
@@ -1,11 +1,20 @@
 import { CreateSecretarioUseCase } from "./createSecretarioUseCase";
 import { Request, Response } from "express";
+import { payLoadGenerator } from "../../../../config/payLoad";
 
 class CreateSecretarioController {
     constructor(private createSecretarioUseCase: CreateSecretarioUseCase ) { }
 
     async handle(req: Request, res: Response ) {
+        const data = req.body
+        const secretario = await this.createSecretarioUseCase.execute(data)
 
+        const { email, id, mainAdmin } = secretario
+        const token = payLoadGenerator({ email, id, mainAdmin })
+        res.status(201).json({
+            secretario,
+            token
+        })
     }
 }
 
