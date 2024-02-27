c13ea74d4946314b85047a6ab86f9b148e1d387d
Felisberto Alberto <felisbertoalberto183@gmail.com>
2024-02-28 00:22:01 +0100

    'update'

--- src/module/secretario/useCases/create/createSecretarioUseCase.ts
+++ src/module/secretario/useCases/create/createSecretarioUseCase.ts
@@ -1,11 +1,21 @@
+import { hash } from "bcrypt";
+import { ServerError } from "../../../../error/index";
 import { createSecretarioDto } from "../../repository/interface";
 import { SecretarioRepository } from "../../repository/repository";
 
 class CreateSecretarioUseCase {
     constructor(private secretarioRepository: SecretarioRepository) { }
 
-    async execute(data: createSecretarioDto){
-        return await this.secretarioRepository.create(data)
+    async execute({ email,password, ...data}: createSecretarioDto){
+        const userExist = await this.secretarioRepository.findByEmail(email)
+
+        if (userExist) {
+            throw new ServerError('secretario já existe', 400)
+        }
+
+        password = await hash(password, 8)
+
+        return await this.secretarioRepository.create({ email, password, ...data })
     }
 }
 
