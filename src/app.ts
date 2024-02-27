39f9286464f044ad0cf62b14245ed971dcf2bb35
Felisberto Alberto <felisbertoalberto183@gmail.com>
2024-02-28 00:07:26 +0100

    'update'

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import * as dotenv from "dotenv";
+dotenv.config()
+
+
+const app = express()
+
+
+
+export { app }
+
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { app } from "app";
+
+
+const PORT = process.env.PORT || 3334
+app.listen(PORT,()=> console.log('rodando na porta' + PORT))
