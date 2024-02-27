aa95eba04cb364411531f89ee57fea4a9087d12a
Felisberto Alberto <felisbertoalberto183@gmail.com>
2024-02-28 00:55:18 +0100

    'update'

--- src/module/secretario/useCases/create/createSecretarioController.ts
+++ src/module/secretario/useCases/create/createSecretarioController.ts
@@ -1,21 +1,30 @@
 import { CreateSecretarioUseCase } from "./createSecretarioUseCase";
 import { Request, Response } from "express";
 import { payLoadGenerator } from "../../../../config/payLoad";
+import { createsecretarioSchema } from "../../../../config/yup";
+import { ServerError } from "error";
 
 class CreateSecretarioController {
     constructor(private createSecretarioUseCase: CreateSecretarioUseCase ) { }
 
     async handle(req: Request, res: Response ) {
         const data = req.body
-        const secretario = await this.createSecretarioUseCase.execute(data)
 
-        const { email, id, mainAdmin } = secretario
-        const token = payLoadGenerator({ email, id, mainAdmin })
-        
-        res.status(201).json({
-            secretario,
-            token
-        })
+        try {
+            await createsecretarioSchema.validate(data)
+
+            const secretario = await this.createSecretarioUseCase.execute(data)
+    
+            const { email, id, mainAdmin } = secretario
+            const token = payLoadGenerator({ email, id, mainAdmin })
+            
+            res.status(201).json({
+                secretario,
+                token
+            })
+        } catch (error: any) {
+            throw new ServerError(error.message, 400)
+        }
     }
 }
 
--- src/router/index.ts
+++ src/router/index.ts
@@ -3,10 +3,6 @@ import { secretarioRouter } from "./secretarios.routes";
 
 const router = Router()
 
-// router.get((req:Request, res:Response)=>{
-//     console.log('valeu')
-// }
-// )
 router.use('/secretario',secretarioRouter)
 
 export { router }
