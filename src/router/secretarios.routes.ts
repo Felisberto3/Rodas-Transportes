1b581e691a5f4caa24ee7dccbbdd1c052736f0a7
Felisberto Alberto <felisbertoalberto183@gmail.com>
2024-02-28 00:36:07 +0100

    'update'

--- src/router/secretarios.routes.ts
+++ src/router/secretarios.routes.ts
@@ -1,7 +1,8 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
+import { createSecretario } from "../module/secretario/useCases/create/index";
 
 const secretarioRouter = Router()
 
 secretarioRouter.post('/create', async (req:Request, res: Response) => {
-    // return awai
+    return await createSecretario.handle(req,res)
 })
