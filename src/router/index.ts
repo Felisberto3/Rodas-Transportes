f76f05a597acfd97452e5f8591ca21c3aa0d4e12
Felisberto Alberto <felisbertoalberto183@gmail.com>
2024-02-28 02:31:51 +0100

    'update'

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,12 +2,14 @@ import { Router } from "express";
 import { secretarioRouter } from "./secretarios.routes";
 import { propinaRouter } from "./propina.routes";
 import { multaRouter } from "./multa.routes";
+import { addressRouter } from "./address.routes";
 
 const router = Router()
 
 router.use('/secretario',secretarioRouter)
 router.use('/propina',propinaRouter)
 router.use('/multa',multaRouter)
+router.use('/address',addressRouter)
 
 
 export { router }
